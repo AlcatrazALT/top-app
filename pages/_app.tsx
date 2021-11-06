f802db86fb83557022192e458c840f6137351559
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2021-11-06 02:28:39 -0500

    fix: Fix head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import React from 'react';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Top list</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
