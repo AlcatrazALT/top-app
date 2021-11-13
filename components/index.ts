de4f08d1e482222f4dc547c550bdc3d87512f9db
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2021-11-12 21:47:10 -0500

    feat: Add button component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+import Button from "./Button/Button";
 import Htag from "./Htag/Htag";
 
-export { Htag };
+export { Htag, Button };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 import { HtagType } from "../components/Htag/Htag.props";
 
 export default function Home() {
   return (
     <>
       <Htag tag={HtagType.h1}>Text</Htag>
+      <Button appearance='primary'>Click</Button>
+      <Button appearance='ghost'>Click</Button>
     </>
   );
 }
