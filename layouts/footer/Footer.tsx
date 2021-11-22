1fe0334f40eb977af89bdff7d1c86ca6b040bdf3
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2021-11-22 08:51:04 -0500

    feat: Add grid to footer

--- layouts/footer/Footer.tsx
+++ layouts/footer/Footer.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import {format} from 'date-fns';
 
-const Footer = ({...props}: FooterProps) => {
+const Footer = ({className, ...props}: FooterProps) => {
   return (
-    <div {...props}>
-      <h4>Footer</h4>
-    </div>
+    <footer
+      className={cn(
+        className,
+        styles.footer,
+        )}
+      {...props}
+      >
+      <div>
+        2020 - {format(new Date(), 'yyyy')} All rights reserved
+      </div>
+      <a href="#" target="_black">User agreement</a>
+      <a href="#" target="_black">Privacy policy</a>
+    </footer>
   );
 };
 
