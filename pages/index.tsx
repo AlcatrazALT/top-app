c1124ba3b90529c5cf2ea7cf38cc11dd9d7281ea
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2021-11-26 07:56:44 -0500

    feat: use getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
+import axios from "axios";
+import { GetStaticProps } from "next";
 import React, { useState } from "react";
-import { Button, Paragraph, Rating, Tag } from "../components";
 import { withLayoutHOC } from "../layouts";
+import { MenuItem } from "../types/menu.interface";
 
-const Home = () => {
-  const [rating, setRating] = useState(4);
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
 
+const Home = ({menu, firstCategory}: HomeProps) => {
   return (
     <>
-      <h4>Body</h4>
+      <ul>
+        {menu.map((m, i) => (<li key={i}>{m._id.secondCategory}</li>))}
+      </ul>
     </>
   );
 };
 
 export default withLayoutHOC(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const {data:menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu, firstCategory
+    }
+  };
+};
