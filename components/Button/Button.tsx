d89cf5509bba54c804ed7ba3f3fab796ba61697b
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2022-01-15 00:06:49 -0500

    feat: button animation

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
 import ArrowIcon from './arrow.svg';
+import { motion, useMotionValue } from 'framer-motion';
 
 const Button = ({appearance, arrow = 'none', children, className, ...props}: ButtonProps) => {
+  const scale = useMotionValue(1);
+  
+  useEffect(() => {
+    scale.onChange(scale => console.log(scale));
+  }, []);
+
   return (
-    <button
+    <motion.button
       className={cn(
         className,
         styles.button, {
         [styles.primary]: appearance === 'primary',
         [styles.ghost]: appearance === 'ghost'})}
+      whileHover={{scale:1.05}}
+      style={{scale}}
       {...props}
       >
       {children}
@@ -23,7 +32,7 @@ const Button = ({appearance, arrow = 'none', children, className, ...props}: But
         >
           <ArrowIcon />
         </span>}
-    </button>
+    </motion.button>
   );
 };
 
